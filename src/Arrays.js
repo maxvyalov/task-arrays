aad1e7210990c3328a797bb14056f0bf5e799e6b
maxvyalov <maxvyalovyar@gmail.com>
2022-03-28 21:38:22 +0300

    Задание по массивам

--- src/Arrays.js
+++ src/Arrays.js
@@ -1,20 +1,31 @@
-/*
-  В функцию arraySorting() приходит строка чисел с разделительными запятыми вида "1,2,33,0,17".
-  Преобразуте строку в массив чисел, отсортируйте их по возрастанию и верните строку с упорядоченными числами,
-  разделенными запятыми.
-*/
-export function arraySorting(data) {}
+import { resolveSoa } from 'dns';
 
-/*
-  В функцию arrayFiltering() приходит массив целых чисел.
-  Преобразуйте массив таким образом, чтобы все элементы массива, в которых содержатся числа более 100,
-  были бы удалены из массива, и верните его в качестве результата функции.
-*/
-export function arrayFiltering(data) {}
+export function arraySorting(data) {
+    data = data.split(',');
+    let res = data.sort((a, b) => a - b);
+    return res.toString();
+}
+
+export function arrayFiltering(data) {
+    let res = [];
+    for (let i = 0; i < data.length; i++) {
+        if (data[i] < 100) {
+            res.push(data[i]);
+        }
+    }
+    return (data = res);
+}
 
 /*
   В функцию arrayPushing() приходят два массива целых чисел размерностью 5 элементов.
   Получите третий массив размерностью 10 элементов, в котором последовательно чередуются
   значения из первых двух массивов, и верните его в качестве результата функции.
 */
-export function arrayPushing(array1, array2) {}
+export function arrayPushing(array1, array2) {
+    let res = [];
+    for (let i = 0; i < array1.length; i++) {
+        res.push(array1[i]);
+        res.push(array2[i]);
+    }
+    return res;
+}
